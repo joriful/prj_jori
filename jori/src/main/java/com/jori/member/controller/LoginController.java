68a992a336742500893d47cf81418f3dad73fa40
joriful <jorimi9996@gmail.com>
2017-12-19 22:42:03 +0900

    login requestmapping

--- jori/src/main/java/com/jori/member/controller/LoginController.java
+++ jori/src/main/java/com/jori/member/controller/LoginController.java
@@ -1,11 +1,20 @@
 package com.jori.member.controller;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Locale;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.jori.member.model.LoginInfo;
 import com.jori.member.model.MloginDAO;
 
 @Controller
@@ -21,6 +30,30 @@ public class LoginController {
 		mav.setViewName(page);
 		
 		return mav;
+	}
+	
+	@RequestMapping(value="/login/loginChk")
+	public void loginChk(Locale locale, Model model, LoginInfo loginInfo, HttpSession session, HttpServletResponse response) throws IOException{
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter out=response.getWriter();
+		
+		if((loginInfo.getId() != null && !loginInfo.getId().equals("")
+				&& loginInfo.getPassword() != null && !loginInfo.getPassword().equals(""))) {
+			
+			if(loginDAO.loginChk(loginInfo)) {
+				session.setAttribute("login", 0);//로그인 성공시 세션
+				
+				session.setAttribute("id", loginInfo.getId());
+				out.println("<script>location.href='/'; </script>");
+				out.flush();
+				out.close();
+			}
+			if(loginDAO.loginChk(loginInfo) == false) {
+				out.print("<script>alert('로그인 정보를 확인하세요!'); history.go(-1); </script>");
+				out.flush();
+				out.close();
+			}
+		}
 	}
 	
 }//
